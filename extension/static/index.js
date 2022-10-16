567cd8b1b393389bb9c16e370fcbef98926207cf
aaminaali <92750667+aaminaali@users.noreply.github.com>
2022-10-16 18:51:54 -0500

    Update index.js

--- extension/static/index.js
+++ extension/static/index.js
@@ -1,5 +1,7 @@
 const btn = document.getElementById('btn');
+
 btn.addEventListener('click', function onClick(event) {
-  //Change text color on entire webpage
+  // Change text color on entire webpage
   document.body.style.color = 'red';
+  
 });
