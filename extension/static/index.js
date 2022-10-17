e827168edc00df2ee34f380f5d6a1e7a7a16db16
aaminaali <92750667+aaminaali@users.noreply.github.com>
2022-10-16 19:15:48 -0500

    Update index.js

--- extension/static/index.js
+++ extension/static/index.js
@@ -1,14 +1,4 @@
-<html>
-<head>
-    <title>Change Text Color</title>
-</head>
-<body>
-<button id="changeRed">Change Font Color Red</button>
-<script>
-  
-document.getElementById("changeRed").onclick = function(){
-	document.getElementById("output").style.color = 'red';
-}
-</script>
-</body>
-</html>
+const btn = document.getElementById('btn');
+btn.addEventListener('click', function onClick(event) {
+  document.body.style.color = 'red';
+});
