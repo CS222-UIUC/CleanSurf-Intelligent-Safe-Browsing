2fa5a9ce55eebae71d106e7fc21aa37384c9b923
timor <timorea2@illinois.edu>
2022-11-15 18:45:09 -0600

    Made processor class non-static

--- api/image_processor.py
+++ api/image_processor.py
@@ -8,12 +8,14 @@ classifier = NudeClassifier()
 
 
 class ImageProcessor:
-    @staticmethod
-    def process(image: Image) -> Image:
+    def __init__(self, threshold: float = 0.5):
+        self.threshold = threshold
+
+    def process(self, image: Image) -> Image:
         # Blurring the image
         image_path = 'temp_{}.jpeg'.format(time.time())
         image.save(image_path)
-        if classifier.classify(image_path)[image_path]['unsafe'] > 0.5:
+        if classifier.classify(image_path)[image_path]['unsafe'] > self.threshold:
             processed_image = image.filter(ImageFilter.GaussianBlur(radius=10))
         else:
             processed_image = image
