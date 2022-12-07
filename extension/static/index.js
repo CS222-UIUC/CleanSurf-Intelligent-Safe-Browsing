e68c3487b467a0096c3880d13893cc914a063f2c
timor <timorea2@illinois.edu>
2022-12-07 11:50:48 -0600

    Removed animate button function

--- extension/static/index.js
+++ extension/static/index.js
@@ -4,29 +4,17 @@
  * @param {string} message
  * @param {function} onReceive
  */
-function sendMessage(message, onReceive) {
+function sendMessage(message) {
   console.log(message);
   chrome.tabs.query({active: true, currentWindow: true}, (tabs) => {
     chrome.tabs.sendMessage(tabs[0].id, {status: message}, () => {});
   });
 }
 
-/**
- * Animates the button
- */
-function animateButton() {
-  console.log('Animating button');
-  const button = document.getElementById('clean-btn');
-  button.classList.add('animate');
-  setTimeout(() => {
-    button.classList.remove('animate');
-  }, 1000);
-}
-
 document.addEventListener('DOMContentLoaded', function() {
   const btn = document.getElementById('clean-btn');
   btn.addEventListener('click', function onClick(event) {
-    sendMessage('clean', animateButton);
+    sendMessage('clean');
   });
 });
 
