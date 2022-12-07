129e1c2e819418239bde5f0500297dc331119cb8
timor <timorea2@illinois.edu>
2022-12-07 00:11:03 -0600

    Added loading effect to screen

--- extension/static/index.js
+++ extension/static/index.js
@@ -2,18 +2,34 @@
  * Sends a message to the content script
  *
  * @param {string} message
+ * @param {function} onReceive
  */
-function sendMessage(message) {
+function sendMessage(message, onReceive) {
   console.log(message);
   chrome.tabs.query({active: true, currentWindow: true}, (tabs) => {
     chrome.tabs.sendMessage(tabs[0].id, {status: message}, () => {});
   });
 }
 
-const btn = document.getElementById('clean-btn');
-btn.addEventListener('click', function onClick(event) {
-  sendMessage('clean');
+/**
+ * Animates the button
+ */
+function animateButton() {
+  console.log('Animating button');
+  const button = document.getElementById('clean-btn');
+  button.classList.add('animate');
+  setTimeout(() => {
+    button.classList.remove('animate');
+  }, 1000);
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+  const btn = document.getElementById('clean-btn');
+  btn.addEventListener('click', function onClick(event) {
+    sendMessage('clean', animateButton);
+  });
 });
+
 
 const slider = document.getElementById('intensity-slider');
 slider.addEventListener('input', () => {
